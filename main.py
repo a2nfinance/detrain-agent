04bff363e4368834d7894c809116d73a401634d1
a2nfinance <levi@a2n.finance>
2024-05-09 21:57:09 +0700

    add agents

--- main.py
+++ main.py
@@ -39,6 +39,6 @@ async def download_file(request: Request):
 
 if __name__ == "__main__":
     print("Starting DeTrain agent")
-    subprocess.Popen("fuser -k 5000/tcp", stdout=subprocess.PIPE, shell=True)
-    subprocess.Popen("killall gunicorn; gunicorn -w 1 -k uvicorn.workers.UvicornWorker --bind 0.0.0.0:5000 main:app --daemon", stdout=subprocess.PIPE, shell=True)
+    # subprocess.Popen("fuser -k 5000/tcp;killall gunicorn", stdout=subprocess.PIPE, shell=True)
+    subprocess.Popen("gunicorn -w 1 -k uvicorn.workers.UvicornWorker --bind 0.0.0.0:5000 main:app --daemon", stdout=subprocess.PIPE, shell=True)
     print("Agent is started")
